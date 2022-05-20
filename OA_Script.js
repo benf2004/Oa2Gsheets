e16203d1416134bbdcea240c308d321f423b495c
benfinch04 <benfinch04@gmail.com>
2022-05-19 22:19:54 -0600

    reformatting list

--- OA_Script.js
+++ OA_Script.js
@@ -84,8 +84,15 @@
       //var margin = "=" + columnDict['profit'] + rowStr + "/" + columnDict['price'] + rowStr;
       var roi1 = "=" + columnDict['profit'] + rowStr + "/" + columnDict['cogs'] + rowStr;
       //var proceeds = "=" + columnDict['price'] + "-" + columnDict['totFees'] + rowStr;
-      const infoArray = [curDate, asinLink, title, roi1, currentRank, cate, sourceURL, cogs, price, profit1, refPer, notes, refFee1, pickPack, totFees1, sellLink];
-      return infoArray
+      const my_list = [curDate, asinLink, title, roi1, currentRank, cate, sourceURL, cogs, price, profit1, refPer, notes, refFee1, pickPack, totFees1, sellLink];
+      var new_item, new_list;
+      new_list = [];
+      for (var each, _pj_c = 0, _pj_a = my_list, _pj_b = _pj_a.length; _pj_c < _pj_b; _pj_c += 1) {
+        each = _pj_a[_pj_c];
+        new_item = [my_list[each]];
+        new_list.append(new_item);
+      }
+      return new_list
 	}; // end of dynamic stats function
 	var infoSend = dynamStats(checker)
     return infoSend
@@ -113,7 +120,7 @@
         }
 
         var valueRangeBody = {
-            "majorDimension": "ROWS",
+            "majorDimension": "COLLUMNS",
             "range": range,
             "values": send
         }
