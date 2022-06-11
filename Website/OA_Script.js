8f6a777bef1c03a13a595107d815324c38fb53f8
benfinch <benfinch04@gmail.com>
2022-06-11 15:53:20 -0600

    redoing ordering!

--- Website/OA_Script.js
+++ Website/OA_Script.js
@@ -22,8 +22,8 @@
     var object = await keepa(asin);
 	var product = object['products'][0];
 	var title = product['title'];
-	var currentStats = object['products'][0]['stats']['current'];  
-    
+	var currentStats = object['products'][0]['stats']['current'];
+
 	// Create variables for desired statistics from Keepa JSON
 	var currentRank = currentStats[3];
 	var fbaFees = product["fbaFees"];
@@ -32,46 +32,46 @@
     var cate = cats2[1]['name']
 	var today = new Date();
 	var curDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-	var asinLink = '=HYPERLINK("amazon.com/dp/' + asin + '"' + "," + '"' + asin + '"' + ")" 
-	var sellLink = "https://sellercentral.amazon.com/product-search/search?q=" + asin 
-    
+	var asinLink = '=HYPERLINK("amazon.com/dp/' + asin + '"' + "," + '"' + asin + '"' + ")"
+	var sellLink = "https://sellercentral.amazon.com/product-search/search?q=" + asin
+
     // decodes & recieves variables sent from query attributes in url
     var price = decodeURI(urlParams.get("price"))
     // console.log("price is: " + price)
 	var cogs = decodeURI(urlParams.get("cogs"))
-    // console.log("cogs is: " + cogs)	
+    // console.log("cogs is: " + cogs)
 	var sourceURL = decodeURI(urlParams.get("sourceurl"))
-    // console.log("source url is: " + sourceURL)	
+    // console.log("source url is: " + sourceURL)
 	var notes = decodeURI(urlParams.get("notes"))
     // console.log("notes is: " + notes)
           
 
 	// Sets dynamic statistic array to send to spreadsheet. requires row number.
         // TODO: build new columnDict with order
-	function dynamStats(rowStr, order){
-        var refPer = decodeURI(urlParams.get("refPer"))
-        const alpha_dict = { }
-        const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-        let num = 0
-        for (let each of order){
-            alpha_dict[each] = alphabet[num]
-            num += 1
-        }
-        const a = alpha_dict
-        console.log("AlPHA DICT:" + a)
+        function dynamStats(rowStr, order) {
+            var refPer = decodeURI(urlParams.get("refPer"))
+            const alpha_dict = {}
+            const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+            let num = 0
+            for (let each of order) {
+                alpha_dict[each] = alphabet[num]
+                num += 1
+            }
+            const a = alpha_dict
+            console.log("AlPHA DICT:" + a)
 
-      let refFee1 = "=" + a['8'] + rowStr + "*" + a['10'] + rowStr;
-      let other_fees = 0;
-      let profit1 = "=" + a['8'] + rowStr + "-" + a['14'] + rowStr + "-" + a['7'] + rowStr;
-      let totFees1 = "=" + a['12'] + rowStr + "+" + a['13'] + rowStr;
-      let margin = "=" + a['16'] + rowStr + "/" + a['8'] + rowStr;
-      let sales_tax = 0;
-      let roi1 = "=" + a['9'] + rowStr + "/" + a['7'] + rowStr;
-      let proceeds = "=" + a['8'] + "-" + a['14'] + rowStr;
-      const my_list = [curDate, asinLink, title, roi1, currentRank, cate, sourceURL, cogs, price, profit1, refPer, notes, refFee1, pickPack, totFees1, sellLink, margin, other_fees, sales_tax, proceeds];
-      console.log(my_list)
-    return my_list
-	}; // end of dynamic stats function
+            let refFee1 = "=" + a['8'] + rowStr + "*" + a['10'] + rowStr;
+            let other_fees = 0;
+            let profit1 = "=" + a['8'] + rowStr + "-" + a['14'] + rowStr + "-" + a['7'] + rowStr;
+            let totFees1 = "=" + a['12'] + rowStr + "+" + a['13'] + rowStr;
+            let margin = "=" + a['16'] + rowStr + "/" + a['8'] + rowStr;
+            let sales_tax = 0;
+            let roi1 = "=" + a['9'] + rowStr + "/" + a['7'] + rowStr;
+            let proceeds = "=" + a['8'] + "-" + a['14'] + rowStr;
+            const my_list = [curDate, asinLink, title, roi1, currentRank, cate, sourceURL, cogs, price, profit1, refPer, notes, refFee1, pickPack, totFees1, sellLink, margin, other_fees, sales_tax, proceeds];
+            console.log(my_list)
+            return my_list
+        }; // end of dynamic stats function
 
 	var infoSend = dynamStats(checker)
     return infoSend
@@ -84,7 +84,15 @@
         let rowNum = data2["values"].length + 1
         return rowNum;
     };
-    
+
+    function orderSend(data, order){
+        const send = []
+        for (let each of order){
+            send.push(data[each])
+        }
+        return send
+    }
+
     // sends info to Google Sheets via Gsheets API
     function sendToSheets(send, range) {
         var params = {
@@ -124,11 +132,13 @@
     // checks if ASIN is included in URL. If yes, calls main functions in relevant order.
     if (asin != "") {
         var fileID = decodeURI(urlParams.get("fileID"))
+        const order = JSON.parse(decodeURI(urlParams.get('order')))
         console.log("spreadsheet id:" + fileID)  // gets spreadsheet id num
         let rowFin = await getRowNum(); // gets row number
-        let data1 = await main(rowFin); // gets statistics 
-        var range1 = "A" + rowFin + ":P" + rowFin // creates range from row number
-        sendToSheets(data1, range1); // sends data to gsheets
+        let data1 = await main(rowFin); // gets statistics
+        let send_data  = orderSend(data1, order)
+        let range1 = "A" + rowFin + ":P" + rowFin // creates range from row number
+        sendToSheets(send_data, range1); // sends data to gsheets
     } 
     else {
         console.log("error: no parameters recieved") // logs error in console if no ASIN is recieved in URL
