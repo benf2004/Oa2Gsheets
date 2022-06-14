652130b2c826d03f5b55c560517c765a03a477c4
benfinch <benfinch04@gmail.com>
2022-06-13 20:59:45 -0600

    picker fix, implementing extension

--- Chrome_Extension/AMZscript.js
+++ Chrome_Extension/AMZscript.js
@@ -2,11 +2,14 @@ fetch(chrome.runtime.getURL('/amz.html')).then(r => r.text()).then(html => {
     let title = document.getElementById('title_feature_div')
     title.insertAdjacentHTML('afterend', html);
     chrome.storage.sync.get(['fileID'], function (result) {
-        chrome.storage.sync.get(['order'], function (order){
-            let fileID = result
-            let my_order = order
+        const fileID = result
+        chrome.storage.sync.get(['order'], function (result){
+            console.log(result)
+            const my_order = result
+            console.log(order)
             let source = "https://www.oa2gsheets.com/Website/input?fileID" + fileID + "&o=" + my_order
-            let frame1 = document.getElementById("send")
+            console.log(source)
+            let frame1 = document.getElementById("send_oa2gsheets")
             frame1.setAttribute("src", source)
         });
     });
