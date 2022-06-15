09d9e1de5eb4526d2a6e791a46daef6df25022c0
benfinch <benfinch04@gmail.com>
2022-06-15 12:57:44 -0600

    handle variation popstate

--- Chrome_Extension/AMZscript.js
+++ Chrome_Extension/AMZscript.js
@@ -29,23 +29,29 @@ function is_product_page(url) {
     return return_val
 }
 
-fetch(chrome.runtime.getURL('/amz.html')).then(r => r.text()).then(html => {
-    var url1 = document.location.href
-    let title = document.getElementById('title_feature_div')
-    title.insertAdjacentHTML('afterend', html);
-    chrome.storage.sync.get(['fileID'], function (result) {
-        const fileID = result.fileID
-        console.log(fileID)
-        chrome.storage.sync.get(['order'], function (result) {
-            let asin = getASIN(url1)
-            console.log(result)
-            const my_order = result.order
-            console.log(my_order)
-            let source = "https://www.oa2gsheets.com/Website/input?fileID=" + fileID + "&o=" + my_order + "&asin=" + asin
-            console.log("source is" + source)
-            let frame1 = document.getElementById("input_oa2gsheets")
-            frame1.setAttribute("src", source)
+function main () {
+    fetch(chrome.runtime.getURL('/amz.html')).then(r => r.text()).then(html => {
+        var url1 = document.location.href
+        let title = document.getElementById('title_feature_div')
+        title.insertAdjacentHTML('afterend', html);
+        chrome.storage.sync.get(['fileID'], function (result) {
+            const fileID = result.fileID
+            console.log(fileID)
+            chrome.storage.sync.get(['order'], function (result) {
+                let asin = getASIN(url1)
+                console.log(result)
+                const my_order = result.order
+                console.log(my_order)
+                let source = "https://www.oa2gsheets.com/Website/input?fileID=" + fileID + "&o=" + my_order + "&asin=" + asin
+                console.log("source is" + source)
+                let frame1 = document.getElementById("input_oa2gsheets")
+                frame1.setAttribute("src", source)
+            });
         });
     });
-  });
+}
 
+window.addEventListener('popstate', function (event) {
+    comsole.log("URL CHANGED")
+    main()
+});
