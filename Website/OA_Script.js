849bd6e39dcf52481db86aced59ca6142ff8003d
benfinch <benfinch04@gmail.com>
2022-06-14 20:19:40 -0600

    trying to fix row num :/

--- Website/OA_Script.js
+++ Website/OA_Script.js
@@ -91,9 +91,9 @@
         var request = gapi.client.sheets.spreadsheets.values.get(params);
         request.then(function(response) {
             // TODO: Change code below to process the `response` object:
-            const data = response
+            const data = response.result
             console.log(data)
-            let rowNum = data["values"].length + 1
+            let rowNum = (data.values).length + 1
             console.log(rowNum)
             document.cookie = "row=" + rowNum;
             return rowNum
