1e75ea093c47c6d8e6170f644630e73a0d69c6fc
benfinch <benfinch04@gmail.com>
2022-06-17 15:38:44 -0600

    store cookies update

--- Chrome_Extension/background.js
+++ Chrome_Extension/background.js
@@ -7,8 +7,6 @@ extpay.startBackground();
 
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
-        // read changeInfo data and do something with it
-        // like send the new url to contentscripts.js
         if (changeInfo.url) {
             chrome.tabs.sendMessage( tabId, {
                 message: 'url_change',
@@ -17,3 +15,45 @@ chrome.tabs.onUpdated.addListener(
         }
     }
 );
+
+function getCookies(domain, name, callback) {
+    chrome.cookies.get({"url": domain, "name": name}, function (cookie) {
+        if (callback) {
+            callback(cookie.value);
+        }
+    });
+}
+
+const website = 'http://oa2gsheets.com/Website'
+const filter = {
+    url: [
+        {
+            urlMatches: 'https://oa2gsheets.com/Website/picker',
+        },
+    ],
+};
+
+chrome.webNavigation.onCommitted.addListener(() => {
+    console.log("TRIGGERED")
+    getCookies(website, "fileID", function (id) {
+        console.log(id);
+        chrome.storage.sync.set({fileID: id}, function () {
+            console.log('Value is set to ' + id);
+        });
+    });
+
+    getCookies(website,"order", function (id){
+        console.log(id);
+        chrome.storage.sync.set({order: id}, function(){
+            console.log("Success adding order to sync!")
+            console.log(id)
+        })
+    })
+
+    getCookies(website, 'is_dynam', function(id){
+        console.log(id)
+        chrome.storage.sync.set({is_dynam: id}, function(){
+            console.log("Success adding is_dynam to sync!")
+        })
+    })
+}, filter);
